1afdbefea77ead59868f98be0a7b859c17d69205
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2025-04-28 22:17:30 -0400

    Update main1.c

--- test/main1.c
+++ test/main1.c
@@ -1,1 +1,13 @@
+Game createGame(int r, char *gameTitle);
+void printGameInfo(const Game *game);
 
+int main() {
+    Game g = createGame(100, "Witcher 3");
+    Game g2 = createGame(75, "Mario Brothers");
+    Game g3 = createGame(100, "GTA 6");
+    printGameInfo(&g);
+    printGameInfo(&g2);
+    printGameInfo(&g3);
+
+    return 0;
+}
